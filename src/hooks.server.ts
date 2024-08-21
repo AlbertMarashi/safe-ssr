712a2248b9a6f09ae1d9b796d92efc38576fd83d
Albert <albert@lumina.earth>
2024-08-22 02:32:51 +0930

    remove log

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,6 @@
 import { safe_request_wrapper } from "$lib/safe_request_wrapper.js"
 import { sequence } from "@sveltejs/kit/hooks"
-import { app_state } from "./routes/app.js"
-import request_symbol from "$lib/request_symbol.js"
 
 export const handle = sequence(
     safe_request_wrapper,
-    ({ event, resolve }) => {
-        console.log(request_symbol.current())
-        return resolve(event)
-    }
 )
